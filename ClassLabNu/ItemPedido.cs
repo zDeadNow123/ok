bd8221aafb59c0f388315eeb0b8bb5da78bc4acf
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2022-04-08 10:07:53 -0300

    Classes itemPedido e Pedido

--- ClassLabNu/ItemPedido.cs
+++ ClassLabNu/ItemPedido.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace ClassLabNu
 {
-    internal class ItemPedido
+    public class ItemPedido
     {
+        // idped_ip	idprod_ip	valor	quantidade	desconto
+        // Atributos
+        private Produto produto;
+        private double valor;
+        private double quantidade;
+        private double desconto;
+        
+        // propriedades
+        public Produto Produto { get { return produto; } set { produto = value; } }
+        public double Valor { get { return valor; } set { valor = value; } }
+        public double Quantidade { get { return quantidade; }set { quantidade = value; } }
+        public double Desconto { get { return desconto; }set { desconto = value; } }
+
+        // construtores
+        public ItemPedido() { }
+
+        public ItemPedido(Produto produto, double valor, double quantidade, double desconto)
+        {
+            this.produto = produto;
+            this.valor = valor;
+            this.quantidade = quantidade;
+            this.desconto = desconto;
+        }
+
+        // métodos da classe - Ações da classe
+        public void Inserir() { }
+        public void Alterar(ItemPedido item) 
+        { 
+        
+        }
+
     }
 }
--- ClassLabNu/Pedido.cs
+++ ClassLabNu/Pedido.cs
@@ -8,5 +8,58 @@ namespace ClassLabNu
 {
     public class Pedido
     {
+        // idped	data	status_ped	desconto	idcli_ped	iduser_ped
+        // Atibutos 
+
+        // Propriedades
+        public int Id { get ; set; }
+        public DateTime DataPed { get; set;}
+        public string Status { get; set;}
+        public double Desconto { get; set; }
+        public Cliente Cliente { get; set; }
+        public Usuario Usuario { get; set; }
+        public List<ItemPedido> Itens { get; set; }
+
+        // construtores
+        public Pedido() { }
+
+        public Pedido(DateTime dataPed, string status, double desconto, Cliente cliente, Usuario usuario, List<ItemPedido> itens)
+        {
+            DataPed = dataPed;
+            Status = status;
+            Desconto = desconto;
+            Cliente = cliente;
+            Usuario = usuario;
+            Itens = itens;
+        }
+
+        public Pedido(int id, DateTime dataPed, string status, double desconto, Cliente cliente, Usuario usuario, List<ItemPedido> itens)
+        {
+            Id = id;
+            DataPed = dataPed;
+            Status = status;
+            Desconto = desconto;
+            Cliente = cliente;
+            Usuario = usuario;
+            Itens = itens;
+        }
+        // Métodos da classe - Operações/Açoes/Funções
+        public void Inserir() { }
+        public bool Alterar(Pedido pedido)
+        { 
+            return false;
+        }
+        public static Pedido ConsultarPorId(int _id)
+        { 
+            Pedido pedido = new Pedido(); 
+            // graça...conecta ao banco e realiza a consulta pelo id do pedido
+            return pedido;
+        }
+        public static List<Pedido> ConsultarPorClienteId(int _id)
+        {
+            List<Pedido> pedidos = new List<Pedido>();
+            // graça...conecta ao banco e realiza a consulta pelo id do cliente
+            return pedidos;
+        }
     }
 }
